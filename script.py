9f5040fa6788457a1cfc5d0751d1a979d86a3888
yossefkhader <yossefkhader@campus.technion.ac.il>
2023-01-22 00:55:06 +0200

    first commit

---
+++ script.py
@@ -0,0 +1,123 @@
+import os
+import shutil
+import filecmp
+import difflib
+from ex5 import proccessDiretory
+
+NUM_OF_TESTS = 1001
+testCounter = 0
+
+for num in range(NUM_OF_TESTS):
+    passed = True
+    dirpath1 = os.path.join('tests', "test{}e".format(num))
+    dirpath2 = os.path.join('tests', "test{}d".format(num))
+    
+    print("____________________________________")
+    print("test no.{}:".format(num),end=" ")
+
+    proccessDiretory(dirpath1)
+    
+    if num in [0,1,2,3] :
+        out_file = os.path.join(dirpath1, "test{}.enc".format(num))
+        exp_file = os.path.join(dirpath1, "{}.out".format(num))
+        
+        try:
+            result = filecmp.cmp(out_file, exp_file)
+        except: 
+            passed = False
+            print("FAILED :( -> File Not Found!")
+            print("in comparing: {} - {}".format(file1,file2))
+        if not result:
+            passed = False
+            print("FAILED :(")
+            with open(file1, "r") as f1:
+                text1 = f1.readlines()
+            with open(file2, "r") as f2:
+                text2 = f2.readlines()
+            diff = difflib.unified_diff(text1, text2)
+            print("\n".join(diff))
+
+        src_file = os.path.join(dirpath1, "test{}.enc".format(num))
+        dst_file = os.path.join(dirpath2, "test{}.enc".format(num))
+        shutil.copy(src_file, dst_file)
+        
+        proccessDiretory(dirpath2)
+
+        
+        file1 = os.path.join(dirpath1, "test{}.txt".format(num))
+        file2 = os.path.join(dirpath2, "test{}.txt".format(num))
+        try:
+            result = filecmp.cmp(file1, file2)
+        except: 
+            passed = False
+            print("FAILED :( -> File Not Found!")
+            print("in comparing: {} - {}".format(file1,file2))
+        
+        if not result:
+            passed = False
+            print("FAILED :(")
+            with open(file1, "r") as f1:
+                text1 = f1.readlines()
+            with open(file2, "r") as f2:
+                text2 = f2.readlines()
+            diff = difflib.unified_diff(text1, text2)
+            print("\n".join(diff))
+    
+    else:
+        for elem in os.listdir(dirpath1) :
+            if elem.endswith(".enc") :
+                src_file = os.path.join(dirpath1, elem)
+                dst_file = os.path.join(dirpath2, elem)
+                shutil.copy(src_file, dst_file)
+        
+        proccessDiretory(dirpath2)
+        for elem in os.listdir(dirpath1) :
+            if elem.endswith(".txt") :
+                file1 = os.path.join(dirpath1, elem)
+                file2 = os.path.join(dirpath2, elem)
+                try:
+                    result = filecmp.cmp(file1, file2)
+                except: 
+                    passed = False
+                    print("FAILED :( -> File Not Found!")
+                    print("in comparing: {} - {}".format(file1,file2))
+                    break
+                if not result:
+                    passed = False
+                    print("FAILED :(")
+                    with open(file1, "r") as f1:
+                        text1 = f1.readlines()
+                    with open(file2, "r") as f2:
+                        text2 = f2.readlines()
+                    diff = difflib.unified_diff(text1, text2)
+                    print("\n".join(diff))
+                    break
+    
+    if passed :
+        testCounter += 1
+        print("PASSED :)")
+
+
+
+if testCounter == NUM_OF_TESTS :
+    print("\n\nGood Job! You've PASSED all the tests ")
+else:
+    print("\n\nYou've FAILED in {} tests... Keep going".format(NUM_OF_TESTS-testCounter))
+
+
+
+
+for num in range(NUM_OF_TESTS) :
+    dirpath1 = os.path.join('tests', "test{}e".format(num))
+    dirpath2 = os.path.join('tests', "test{}d".format(num))
+
+    for file in os.listdir(dirpath1) :
+        path = os.path.join(dirpath1, file)
+        if file.endswith(".enc"):
+            os.remove(path)
+    for file in os.listdir(dirpath2) :
+        path = os.path.join(dirpath2, file)
+        if file.endswith(".json"):
+            continue
+        os.remove(path)
+print("deleting files succeeded")
